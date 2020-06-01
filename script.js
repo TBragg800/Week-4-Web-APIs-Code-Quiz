57ebb28e1397cd39513b5a81aecea52fc3edc8c3
LM&T <tbbragg83@gmail.com>
2020-06-01 00:59:50 -0500

    1st commit/ research done/ pages started/ connected pages/ added pseudo code

---
+++ script.js
@@ -0,0 +1,4 @@
+//variables to select start button and access questions
+// questions array/object  
+// addEventListeners on buttons
+// functions for start of quiz, next question and selecting answer
